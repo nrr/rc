2923b446f0fd1ab68aadd87fc2bf78738c00b3cf
Nathaniel R Reindl <nrr@corvidae.org>
2012-03-07 05:45:12 -0600

    Complete the transition from a private Fossil repository to a public git repository at GitHub.

---
+++ README.md
@@ -0,0 +1,18 @@
+This is part of a series. In this particular repository, you will find settings
+for the following:
+
+*  irssi
+*  tinyfugue
+*  RPM building configuration
+*  SSH configuration
+*  GNU Privacy Guard configuration
+*  Bazaar configuration
+*  CVS configuration
+*  Git configuration
+*  Mercurial configuration
+*  tmux configuration
+*  SQLite monitor configuration
+
+To install:
+
+    curl -LsSf https://raw.github.com/nrr/dotfiles/master/move-in.sh | sh -
--- move-in.sh
+++ move-in.sh
@@ -1,36 +1,21 @@
 #!/bin/sh
 #
-# Download the bash environment from fossil.moyi.us and install them to $HOME.
+# General dotfiles.
 #
-# Usage: curl -LsSf http://fossil.moyi.us/nrr-dotfiles-bash-environment/doc/trunk/move-in.sh | sh -
+# See the README for usage.
 #
 
 TD=`mktemp -d XXXXXXX`
-REPO_NAME="dotfiles-bash-environment"
+REPO_OWNER="nrr"
+REPO_NAME="dotfiles"
 
-maybe_create_ssh_directory() {
-	if [ ! -d $HOME/.ssh ]
-	then
-		mkdir -p $HOME/.ssh
-		chmod u=rwx,og-rwx $HOME/.ssh
-	fi
-}
-
-kill_all_history() {
-	for f in .bash_history .mysql_history .psql_history .sqlite_history
-	do
-		rm -f $HOME/$f
-		ln -s /dev/null $HOME/$f
-	done
-}
-
-pull_files_from_fossil() {
+pull_files_from_git() {
 	# This relies on TD above.
 
-	repo_host="chiselapp.com/user/nrr/repository"
-	repo_name=${REPO_NAME}
-	repo_path="/tarball/${repo_name}-tip.tar.gz?uuid=tip"
-	url="http://${repo_host}/${repo_name}/tarball/${repo_name}-tip.tar.gz?uuid=tip"
+	repo_host="github.com"
+	repo_path="${REPO_OWNER}/${REPO_NAME}"
+	archive_path="tarball/master"
+	url="https://${repo_host}/${repo_path}/${archive_path}"
 
 	curl ${CURL_OPTS-""} -LsSf $url | tar -C $TD -xvzf -
 }
@@ -42,12 +27,5 @@ install_into_HOME() {
 	rm -rf $TD
 }
 
-clean_up_old_bashrc_d_entries() {
-	find $HOME/.bashrc.d -type f -size 0 -exec rm -f '{}' \;
-}
-
-pull_files_from_fossil
+pull_files_from_git
 install_into_HOME
-maybe_create_ssh_directory
-kill_all_history
-clean_up_old_bashrc_d_entries
