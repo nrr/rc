dbeb3ca5232530ad5e26b26fc299d3cbb7c6b7a7
Nathaniel Reindl <nrr@corvidae.org>
2015-10-28 12:25:06 -0500

    Add passphrase-laden keys without supervision.

--- .bashrc.d/keychain.sh
+++ .bashrc.d/keychain.sh
@@ -1,6 +1,25 @@
 KEYCHAIN=$(which keychain)
 
+if [ -d $HOME/j/sciolist ]
+then
+	export PATH=$HOME/j/sciolist/bin:$PATH
+fi
+
 if [ -x "$KEYCHAIN" ]
 then
-	eval $("$KEYCHAIN" --eval `find -L ~/.ssh/keys '(' -type f -o -type l ')' -iname 'id_rsa'` ~/.ssh/id_rsa)
+	eval $("$KEYCHAIN" --eval)
+
+	for key in ~/.ssh/id_rsa $(find -L ~/.ssh/keys '(' -type f -o -type l ')' -iname 'id_rsa')
+	do
+		askpass_script=$(mktemp /tmp/askpass.XXXXXX)
+		chmod +x "$askpass_script"
+		cat <<EOF >"$askpass_script"
+#! /usr/bin/env bash
+s6t ssh-askpass "$key"
+EOF
+
+		env DISPLAY= SSH_ASKPASS="$askpass_script" ssh-add $key </dev/null &>/dev/null
+
+		rm "$askpass_script"
+	done
 fi
