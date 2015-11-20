23a52248fa86d0bda252dba4343b5077d063f7ec
Nathaniel Reindl <nrr@corvidae.org>
2015-11-20 18:45:50 -0500

    We don't need most of these anymore.
    Most of them are now managed using nix or some other package manager, so
    manually keeping track of such things (or using tools like perlbrew, et
    al.) is now no longer necessary.

--- .bashrc.d/plan9port.sh
+++
@@ -1,8 +0,0 @@
-PLAN9=/usr/local/plan9
-PATH=$PATH:$PLAN9/bin
-
-# for rc
-home=$HOME
-user=$USER
-
-export PLAN9 PATH home user
--- .bashrc.d/turner.sh
+++
@@ -1,4 +0,0 @@
-if hostname -f | grep -q turner.com
-then
-	export PATH="$HOME/bin/turner:$PATH"
-fi
