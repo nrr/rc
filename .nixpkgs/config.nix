b9d331d294336d25116fe80002c8c261143525b6
Nathaniel Reindl <nrr@corvidae.org>
2015-11-25 10:23:34 -0600

    Flesh out config.nix a bit.

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,3 +1,14 @@
 {
 	allowUnfree = true;
+	packageOverrides = pkgs_: with pkgs_; {
+		all = with pkgs; buildEnv {
+			name = "all";
+			paths = [
+				ledger
+				curl
+				emacs
+				git
+			];
+		};
+	};
 }
