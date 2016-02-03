0d220e0dcb58f3aa31c6cf14dcc442558d914391
Nathaniel Reindl <nrr@corvidae.org>
2016-02-03 08:13:19 -0500

    Git, aliases (shiny!), and scaffolding for OS-specific crap.

--- .bashrc.d/00-base.sh
+++ .bashrc.d/00-base.sh
@@ -6,3 +6,15 @@ then
         source /etc/bashrc
 fi
 
+alias sl='ls' # because I tend to typo this one stupidly often
+alias ls='ls -shF'
+
+
+case $(uname -a) in
+*Linux*)
+	;;
+*Darwin*)
+	;;
+*)
+	;;
+esac
---
+++ .bashrc.d/git.sh
@@ -0,0 +1,1 @@
+export GIT_TEMPLATE_DIR=$HOME/.git_template/
