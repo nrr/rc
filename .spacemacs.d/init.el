87f1f15f4541fd3744e71067f477b72615a67513
Nathaniel Reindl <nrr@corvidae.org>
2017-06-12 12:19:11 -0400

    More Spacemacs changes.
    I honestly thought I'd set this somewhere, but it certainly does explain why my
    git diffs sometimes complain about lacking trailing newlines.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -251,7 +251,8 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (require 'ox-taskjuggler)
-  (setq deft-directory "~/j/notes"))
+  (setq deft-directory "~/j/notes")
+  (setq require-final-newline t))
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
